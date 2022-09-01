bad8a9b75dd5e25b6b7484d657b8f99bd575d46b
Exequiel505 <exequielguardia@gmail.com>
2022-09-01 11:54:23 -0300

    cambio

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-let numMultiplicar = parseInt(prompt("ingrese un numero para multiplicar del 0 al 10"))
+let num = parseInt(prompt("ingrese un numero para multiplicar del 0 al 10"))
+                        //i++=(index = index + 1)
+for(let i=0 ; i <= 10 ; i++ ){ 
 
-for(let i=0 ; i <= 10 ; i++){
+    let resMultiplicacion = num * i
 
-    let resMultiplicacion = numMultiplicar * i
-
-    console.log(`${numMultiplicar} x ${i} = ${resMultiplicacion}`)
+    console.log(`${num} x ${i} = ${resMultiplicacion}`)
 
     //console.log(numMultiplicar.toString() + `x` + i.toString() + `=` + resMultiplicacion.toString())
 
     //alert(`${numMultiplicar} x ${i} = ${resMultiplicacion}`)
 
-    
-    
 } 
