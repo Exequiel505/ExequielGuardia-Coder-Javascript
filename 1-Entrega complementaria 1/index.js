e5e0481dc3698eb6a2350f48cdee924212f621ca
Exequiel505 <exequielguardia@gmail.com>
2022-09-03 01:17:41 -0300

    orden en la rama

---
+++ 1-Entrega complementaria 1/index.js
@@ -0,0 +1,15 @@
+let num = parseInt(prompt("ingrese un numero para multiplicar del 0 al 10"))
+                        //i++=(index = index + 1)
+for(let i=0 ; i <= 10 ; i++ ){ 
+
+    let resMultiplicacion = num * i
+
+    console.log(`${num} x ${i} = ${resMultiplicacion}`)
+
+    //console.log(numMultiplicar.toString() + `x` + i.toString() + `=` + resMultiplicacion.toString())
+
+    //alert(`${numMultiplicar} x ${i} = ${resMultiplicacion}`)
+
+} 
+
+
