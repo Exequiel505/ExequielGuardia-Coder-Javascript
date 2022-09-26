af17706c86d91b5e1e36712123eb7f77a1d489c6
Exequiel505 <exequielguardia@gmail.com>
2022-09-26 14:49:50 -0300

    cambio

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const Pi = 3.14
-console.log(Pi)
+// const Pi = 3.14
+// console.log(Pi)
 
-let Nombre = "Exequiel"
-const Espacio = " "
-let Apellido = "Guardia"
-let Res = Nombre + Espacio + Apellido
-console.log(Res)
+// let Nombre = "Exequiel"
+// const Espacio = " "
+// let Apellido = "Guardia"
+// let Res = Nombre + Espacio + Apellido
+// console.log(Res)
 
-let Marca = "Renault 12" 
-let Precio = 5000
-console.log(`El nuevo ${Marca} sale con un precio de ${Precio} euros`)
+// let Marca = "Renault 12" 
+// let Precio = 5000
+// console.log(`El nuevo ${Marca} sale con un precio de ${Precio} euros`)
 
-let numA = 10
-let numB = 58
-const numC = -5
-let res = numA / numB + numC
-console.log(`el resultado da ${res}`)
+// let numA = 10
+// let numB = 58
+// const numC = -5
+// let res = numA / numB + numC
+// console.log(`el resultado da ${res}`)
 
 /*let pregunta = prompt("¿Usted es mayor de 21 para usar la aplicacion?")
 if(pregunta.toLowerCase() == "si"){
@@ -66,15 +66,31 @@ for(let i=0 ; i <= 10 ; i++ ){
 // let palabra = "cura"
 // palabra = palabra.toLowerCase() 
 
-let usuario = prompt("Ingrese su nombre de usuario").toLowerCase()
+// let usuario = prompt("Ingrese su nombre de usuario").toLowerCase()
 
-let contraseña = parseInt(prompt("Ingrese su contraseña"))
+// let contraseña = parseInt(prompt("Ingrese su contraseña"))
 
-if(usuario == "exequiel" && contraseña === "1234" ){
+// if(usuario == "exequiel" && contraseña === "1234" ){
 
-    alert(`su usuario y contraseña es correcto`)
+//     alert(`su usuario y contraseña es correcto`)
 
-}else{
+// }else{
 
-    alert(`su usuario o contraseña es incorrecto`)
-}
+//     alert(`su usuario o contraseña es incorrecto`)
+// }
+
+function Persona (nombre, edad, calle) {
+
+    this.nombre = nombre;
+    this.edad = edad;
+    this.pepe = calle;
+    
+};
+
+const pers = new Persona("juan", 96, "russia");
+
+// console.log(persona1)
+
+console.log(pers.edad)
+
+document.write(`hola soy ${pers.nombre} tengo ${pers.edad} años y soy de ${pers.pepe}`)
